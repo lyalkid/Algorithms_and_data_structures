8b7fce3c1ffeb203e8fce7cd83dba847a1fda357
lyalkid <121316954+lyalkid@users.noreply.github.com>
2023-02-19 16:29:08 +0300

    Initial commit

---
+++ lab01/src/main/java/ru/itis/lab01/Test.java
@@ -0,0 +1,73 @@
+package ru.itis.lab01;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.EmptyStackException;
+import java.util.Stack;
+public class Test {
+    public static void main(String[] args) {
+        String str = read("transport.json");
+        System.out.println();
+        Stack stack = new Stack();
+        int len = str.length();
+        int i;
+        boolean isGood = true;
+        boolean isCount = false;
+        char l = '"';
+        int count = 0;
+        for(i = 0; i < len; i++){
+            char bracket = str.charAt(i);
+
+            if(bracket == l){
+                count += 1;
+            }
+            if (count == 1 && count != 2){
+                isCount = true;
+            }
+            if(count == 2){
+                count = 0;
+                isCount = false;
+                continue;
+            }
+            if (isCount){
+                continue;
+            }
+            if((bracket == '{') || (bracket == '[') || (bracket == '(')){
+                stack.push(bracket);
+            }
+
+            if((bracket == '}') || (bracket == ']') || (bracket == ')')){
+                try{
+                    char openBracket = (char)stack.pop();
+                    if (openBracket == '(' && bracket == ')'){
+                        continue;
+                    }
+                    if (openBracket == '[' && bracket == ']'){
+                        continue;
+                    }
+                    if (openBracket == '{' && bracket == '}'){
+                        continue;
+                    }
+                } catch(EmptyStackException e){
+                    isGood = false;
+                }
+            }
+
+
+        }
+        System.out.println(isGood && stack.empty() ? "JSON is valid":"JSON is invalid");
+    }
+
+    public static String read(String fileName) {
+        String str = null;
+        try {
+            str = new String(Files.readAllBytes(Paths.get(fileName)));
+        } catch (IOException e) {
+            System.out.println("Ошибка ввода вывода");
+            e.printStackTrace();
+        }
+        return str;
+    }
+}
+
